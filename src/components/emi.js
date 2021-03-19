6067ad382f9cf8232c203874b23876507a0b01ce
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-03-19 16:40:15 +1100

    fresh commit

--- src/components/emi.js
+++ src/components/emi.js
@@ -25,15 +25,15 @@ changeValue(e){
     if(target.value==='5'){
       this.setState({ left: '-5%'})
     }else if(target.value==='10'){
-       this.setState({left: '15%'})
+       this.setState({left: '12%'})
     }else if(target.value==='15'){
-        this.setState({left: '32%'})
+        this.setState({left: '30%'})
     }else if(target.value==='20'){
-       this.setState({left: '50%'})
+       this.setState({left: '49%'})
     }else if(target.value==='25'){
-        this.setState({left: '67%'})
+        this.setState({left: '64%'})
     }else if(target.value==='30'){
-     this.setState({ left:'85%'})
+     this.setState({ left:'81%'})
     }else{
        this.setState({left:'-4%'})
     }
@@ -52,26 +52,32 @@ changeValue(e){
 
    return(
 
+       <div className="slider-container">
+           <label htmlFor="years" className="slider-label">{this.props.label}<GiHourglass className="slider-label-icon"/> </label>
+               <span className='slider-span'style={{left: this.state.left}}>
+                   {this.props.sliderValue} </span>
+                   
+               <input className="slider"
+                   id="years"
+                   type="range"
+                   min="5"
+                   max="30"
+                   step="5"
+                   value={this.props.sliderValue}
+                   onChange={this.changeValue}
+               /> 
+
+        </div>
+    
+ )
+}
+}
+
+export default EMI;
 
           
 
            
-        <div className="slider-container">
-            <label htmlFor="years" className="slider-label">{this.props.label}<GiHourglass className="slider-label-icon"/> </label>
-                <span className='slider-span'style={{left: this.state.left}}>
-                    {this.props.sliderValue} </span>
-                    
-                <input className="slider"
-                    id="years"
-                    type="range"
-                    min="5"
-                    max="30"
-                    step="5"
-                    value={this.props.sliderValue}
-                    onChange={this.changeValue}
-                /> 
-
-         </div>
      
               
                
@@ -90,10 +96,3 @@ changeValue(e){
   
 
 
-
-    
- )
-}
-}
-
-export default EMI;
--- src/components/result.js
+++ src/components/result.js
@@ -4,18 +4,24 @@ import React from 'react';
 const  Result =({emi})=>{
     
 
-
   return(
-         <div className="result">
          
-           <p className="result-details"> Your Monthly Repayments are <br/>${ emi ? Math.round(emi) : 0 }</p>
+      <div className="result">
+         
+        <p className="result-details"> Your Monthly Repayments are<br />
+        <em style={emi? {color: '#28242B'} :{color: '$text-color'}}>${ emi ? Math.round(emi) : 0 }</em></p>
         
+       </div>
            
            
+  )
+}
+    
+export default Result;
 
-         </div>
-       )
 
+
+          
 
 
 
@@ -23,10 +29,6 @@ const  Result =({emi})=>{
            
     
       
-}
-      
-export default Result;
-           
          
              
             
