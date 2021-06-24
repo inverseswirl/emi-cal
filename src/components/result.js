fcf88863d5580777edcf16c73a895b9112e2adbd
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-06-24 15:49:31 +1000

    475px

--- src/components/result.js
+++ src/components/result.js
@@ -8,8 +8,8 @@ const  Result =({emi})=>{
          
       <div className="result">
          
-        <p className="result-details"> Your Monthly Repayments are<br />
-        <em style={emi? {color: '#28242B'} :{color: '$text-color'}}>${ emi ? Math.round(emi) : 0 }</em></p>
+        <p className="result-details"> Your Monthly Repayments are 
+         <em style={emi? {color: '#28242B'} :{color: '$text-color'}}>${ emi ? Math.round(emi) : 0 }</em></p>
         
        </div>
            
